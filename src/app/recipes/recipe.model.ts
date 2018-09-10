b7da216ec8b0943098518fd00ba5e4553a7cd560
Gaurav Sharma <GauravSh@talentica-all.com>
2018-09-10 16:13:18 +0530

    Structure the application

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,10 +1,12 @@
 export class Recipe {
-public name: string;
-public description: string;
-public imagePath: string;
-constructor(name, desc, path) {
-    this.name = name;
-    this.description = desc;
-    this.imagePath = path;
-}
-}
+    public name: string;
+    public description: string;
+    public imagePath: string;
+  
+    constructor(name: string, desc: string, imagePath: string) {
+      this.name = name;
+      this.description = desc;
+      this.imagePath = imagePath;
+    }
+  }
+  
